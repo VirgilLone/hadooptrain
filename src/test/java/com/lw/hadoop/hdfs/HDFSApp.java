b8ebc4572721ac645412653b0abe9ca493361860
luo.wen <wyluowen@163.com>
2019-03-12 16:02:49 +0800

    api操作hdfs

--- src/test/java/com/lw/hadoop/hdfs/HDFSApp.java
+++ src/test/java/com/lw/hadoop/hdfs/HDFSApp.java
@@ -3,13 +3,19 @@ package com.lw.hadoop.hdfs;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Progressable;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
 
 /**
@@ -50,6 +56,66 @@ public class HDFSApp {
         fileSystem.rename(res,tar);
     }
 
+    /**
+     * 本地复制文件到hdfs（上传）
+     */
+    @Test
+    public void copyFromLocalFile() throws Exception{
+        Path localPath = new Path("D:\\copyTest.txt");
+        Path hdfsPath = new Path("/HDFSApi");
+        fileSystem.copyFromLocalFile(localPath,hdfsPath);
+    }
+
+    /**
+     * 带进度上传
+     * @throws Exception
+     */
+    @Test
+    public void copyFromLocalFileWithProgressbar() throws Exception{
+        InputStream inputStream=new BufferedInputStream(new FileInputStream(new File("本地的文件目录")));
+        FSDataOutputStream fsDataOutputStream = fileSystem.create(new Path("/HDFSApi/copyBigFile.tgz"), new Progressable() {
+            @Override
+            public void progress() {
+                System.out.print(".");// 带进度提醒
+            }
+        });
+
+        IOUtils.copyBytes(inputStream,fsDataOutputStream,4096);
+    }
+
+    /**
+     * 下载
+     * @throws Exception
+     */
+    @Test
+    public void copyToLocal() throws Exception{
+        Path localPath = new Path("D:\\");
+        Path hdfsPath = new Path("/test/a/b/h.txt");
+        fileSystem.copyToLocalFile(hdfsPath,localPath);
+    }
+
+    @Test
+    public void delete() throws Exception{
+        fileSystem.delete(new Path("/HDFSApi/aa.txt"),true);
+    }
+
+    /**
+     * 列出文件系统某个目录的所有文件
+     * @throws Exception
+     */
+    @Test
+    public void listHdfsFile() throws Exception{
+        FileStatus[] fileStatuses = fileSystem.listStatus(new Path("/HDFSApi"));
+        for(FileStatus fileStatus : fileStatuses){
+            String isDir=fileStatus.isDirectory()?"文件夹":"文件";
+            long len = fileStatus.getLen();
+            String path = fileStatus.getPath().toString();
+            short replication = fileStatus.getReplication();
+
+            System.out.println(isDir+"\t"+replication+"\t"+path+"\t"+len);
+        }
+
+    }
 
 
 
